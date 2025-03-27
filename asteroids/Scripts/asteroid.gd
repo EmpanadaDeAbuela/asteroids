0f3aeec71b1b616f309d6c6bc73591dd8aa88987
Mateo Fernández <mavafeno@gmail.com>
2025-03-26 21:09:33 -0300

    distintos tamaños de asteroides

--- asteroids/Scripts/asteroid.gd
+++ asteroids/Scripts/asteroid.gd
@@ -4,8 +4,8 @@ var movementVector := Vector2(0, -1)
 
 var speed: int
 
-enum AsteroidSize{LARGE, MEDIUM, SMALL}
-@export var size := AsteroidSize.LARGE
+enum AsSize{LARGE, MEDIUM, SMALL}
+@export var size := AsSize.LARGE
 
 @onready var sprite = $Sprite2D
 
@@ -13,15 +13,20 @@ func _ready() -> void:
 	rotation = randf_range(0, 2*PI)
 	print(rotation_degrees)
 	
-	match size:
-		AsteroidSize.LARGE:
-			speed = randf_range(50, 100)
-			#sprite.texture = dpreload()
-		AsteroidSize.MEDIUM:
-			speed = randf_range(101, 150)
-		AsteroidSize.SMALL:
-			speed = randf_range(151, 200)
-	print(speed)
+	adjustSizes()
+	
 
 func _process(delta: float) -> void:
 	global_position += movementVector.rotated(rotation) * speed * delta
+	
+func adjustSizes():
+	match size:
+		AsSize.LARGE:
+			speed = randf_range(50, 100)
+			#sprite.texture = dpreload()
+		AsSize.MEDIUM:
+			speed = randf_range(101, 150)
+		AsSize.SMALL:
+			speed = randf_range(151, 200)
+	print(speed)
+	
